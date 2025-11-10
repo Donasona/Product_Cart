b7406473c09354e458f308cb1ac36a2c7bcb6f4c
DONA PAUL <donapaul901@gmail.com>
2025-11-10 11:15:06 +0530

    cmt

--- product_app/views.py
+++ product_app/views.py
@@ -59,7 +59,6 @@ class Cartaddview(View):
         id = kwargs.get("pk")
         product = Productmodel.objects.get(id=id)
         Cartmodel.objects.create(product=product,quantity =1)
-        return render(request,"product_list.html")
        
 
     
